59e8203bb3dbfc098c65e56874de76a738ca6d74
GOPI CHAND <119483045+KGC2001@users.noreply.github.com>
2023-08-22 19:14:52 +0530

    Add files via upload

---
+++ mitsogo.js
@@ -0,0 +1,15 @@
+// Scroll to section smoothly when a navigation link is clicked
+// document.querySelectorAll('nav a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         const target = document.querySelector(this.getAttribute('href'));
+//         target.scrollIntoView({
+//             behavior: 'smooth',
+//         });
+//     });
+// });
+
+let cb=document.getElementById("custom-button");
+
+console.log("congrats");
+
